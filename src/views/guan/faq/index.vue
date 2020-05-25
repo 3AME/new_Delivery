77ffcf45afd6b4c5752e3496ea8800a4a4432c50
Jan-spiry <1596343047@qq.com>
2020-05-25 22:34:33 +0800

    faq修复

--- src/views/guan/faq/index.vue
+++ src/views/guan/faq/index.vue
@@ -1,21 +1,4 @@
 <template>
-  <!-- <d2-container >
-    <template slot="header">常见的一些问题</template>
-    <h3 style="color:blue">点击查询结果不显示</h3>
-            <p>
-               解决方案：检查导入文档表头是否与导入文档提示一致，不一致会导致无法获取查询结果.
-            </p>
-            <h3 style="color:blue">如何清除上一次导入的excel文件？</h3>
-            <p>
-                解决方案：点击导入.xlsx/.xls按钮，自动覆盖内容，不会影响下一次查询.
-            </p>
-            <h3 style="color:blue">点击查询以后没有页面跳转？</h3>
-            <p>
-                解决方案：可能是因为运行环境的问题导致响应时间的差异，请耐心等待.也可能是因为数据量大，查询需要一定时间.
-            </p>
-    <template slot="footer">如果你遇到了上述没有提到的问题，并且暂无法解决的话，请将遇到的问题发送到邮箱lafhhh@outlook.com，我们将及时为你解决。</template>
-  </d2-container>-->
-
   <d2-container>
     <!-- <template slot="header">常见的一些问题</template> -->
     <div class="row">
@@ -46,7 +29,6 @@
                           </p>
                     </div>
                 </div>
-              </div>
             </div>
 
             <div class="col-lg-4">
@@ -74,9 +56,9 @@
                 </div>
               </div>
             </div>
-          <!-- </div> -->
-          <!-- <div class="row"> -->
-            <!-- <div class="col-lg-4">
+          </div>
+          <div class="row">
+            <div class="col-lg-4">
               <div class="card faq-box border-danger">
                 <div class="card-body">
                   <div class="faq-icon float-right">
@@ -87,25 +69,23 @@
                   <p class="text-muted mb-0">地图显示不出来可能跟你的网络原因有关，本系统通过调用百度地图API来实现地图显示，请确保你的网络环境良好。</p>
                 </div>
               </div>
-            </div> -->
+            </div>
 
-            <!-- <div class="col-lg-4">
+            <div class="col-lg-4">
               <div class="card faq-box border-info">
                 <div class="card-body">
                   <div class="faq-icon float-right">
                     <i class="fas fa-question-circle font-24 mt-2 text-info"></i>
                   </div>
                   <h5 class="text-info">05.</h5>
-                  <h5 class="font-16 mb-3 mt-4">Why do we use it?</h5>
+                  <h5 class="font-16 mb-3 mt-4">使用地图进行查询结果显示错误</h5>
                   <p class="text-muted mb-0">
-                    Fermentum molestie leo at dignissim mi Donec at ex placerat pretium ex vitae porta orci Pellentesque augue lacus
-                    dignissim tristique aliquam in
-                    placerat feugiat sapien Fusce a enim faucibus lacinia felis a vulputate eros Aenean malesuada et ex et laoreet.
+                    车辆预置有最大里程，地图选择跨省或者大范围的配送点不符合真实情况，程序无法得到解决方案，考虑检查输入.
                   </p>
                 </div>
               </div>
-            </div> -->
-<!-- 
+            </div>
+
             <div class="col-lg-4">
               <div class="card faq-box border-dark">
                 <div class="card-body">
@@ -113,27 +93,18 @@
                     <i class="fas fa-question-circle font-24 mt-2 text-dark"></i>
                   </div>
                   <h5 class="text-dark">06.</h5>
-                  <h5 class="font-16 mb-3 mt-4">Where does it come from?</h5>
+                  <h5 class="font-16 mb-3 mt-4">界面长时间无响应</h5>
                   <p class="text-muted mb-0">
-                    Suspendisse sapien urna ullamcorper vel erat at suscipit dapibus dolor Proin sodales dictum ante Vivamus non dictum metus
-                    Sed quis leo non massa viverra rutrum in a elit Praesent sit amet rhoncus justo Mauris pharetra mollis
-                    enim non efficitur ipsum porttitor vitae.
+                    可能由于格式等问题程序发生错误，顶部导航条提供了界面刷新，可根据自己的需求进行选择.
                   </p>
                 </div>
               </div>
-            </div> -->
-          <!-- </div> -->
+            </div>
+          </div>
           <!-- container-fluid -->
         </div>
       </div>
     </div>
-    <!-- <template slot="footer"> -->
-        <!-- <div class="alert alert-secondary" role="alert" style="background-color: rgb(204,229,255);color:rgb(0,64,133)">
-            如果你遇到了上述没有提到的问题，并且暂无法解决的话，
-            请将遇到的问题发送到邮箱
-            <strong>lafhhh@outlook.com</strong>
-            ，我们将及时为你解决.
-        </div> -->
     <d2-module-index-banner slot="footer" v-bind="banner"/>
     <!-- </template> -->
   </d2-container>
